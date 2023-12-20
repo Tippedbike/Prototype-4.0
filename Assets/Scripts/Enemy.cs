3930f00a9c4a26c8719d7a38b1e2d06b7187574a
sssmart24 <ssmart24@aoiths.org>
2023-12-20 12:44:09 -0500

    4.2 ish

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,7 +18,7 @@ private GameObject player;
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = (player.transform.position - transform.position);
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
     }
 }
